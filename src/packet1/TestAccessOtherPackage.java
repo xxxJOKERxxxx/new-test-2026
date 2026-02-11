a8a4406c3c7eca2a5fddbc3ddd668f4efa34da67
xxxJOKERxxx <0888200@gmail.com>
2026-02-11 23:19:12 +0300

    Extends

--- src/packet1/TestAccessOtherPackage.java
+++ src/packet1/TestAccessOtherPackage.java
@@ -1,4 +1,17 @@
 package packet1;
 
-public class TestAccessOtherPackage {
+import packet2.AccessLevels;
+
+public class TestAccessOtherPackage extends AccessLevels {
+    public static void main(String[] args) {
+        AccessLevels obj = new AccessLevels();
+
+        System.out.println(obj.publicVar);
+
+    }
+
+    public void testProtect() {
+        System.out.println(protectedVar);
+    }
+
 }
