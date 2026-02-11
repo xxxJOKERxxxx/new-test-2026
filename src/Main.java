e2518bc7953b939f6a0175e2c239419736b35e41
xxxJOKERxxx <0888200@gmail.com>
2026-02-11 22:42:19 +0300

    Person

--- src/Main.java
+++ src/Main.java
@@ -3,13 +3,9 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Как зовут вашего друга?");
-        String name = sc.nextLine();
+        Person person1 = new Person();
+        Person person2 = new Person("John");
 
-        System.out.println("Сколько лет вашему другу?");
-        int age = sc.nextInt();
 
-        System.out.println("Моему другу " + name + " сейчас " + age + " лет!");
     }
 }
