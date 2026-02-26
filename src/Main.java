c80cc72731e870e1b5c55900a6beb13309615e75
xxxJOKERxxx <0888200@gmail.com>
2026-02-27 01:51:40 +0300

    МСКЛЮЧЕНИЯ-это и ошибки и не ошибки! бывают контролтруемые бывают двух видов checked и unchecked также errors

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,19 @@
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
 
-
-        Airoplane a = new Airoplane();
-        a.fly();
-
-        Butterfly b = new Butterfly();
-        b.fly();
+        File file = new File("text.txt");
+        try {
+            Scanner scanner = new Scanner(file);
+            System.out.println("Enter text file");
+        } catch (FileNotFoundException e) {
+            System.out.println(e.getMessage());
+        } finally {
+            System.out.println("100%");
+        }
 
     }
 }
